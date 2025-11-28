7b736191db989cd26b03409a4d4dff07262be9b6
Juan <juanurangobarrios@gmail.com>
2025-11-28 14:21:16 -0500

    operaciones aritmecias

--- src/App.java
+++ src/App.java
@@ -15,3 +15,32 @@ class operaciones {
         return a + b;
     }
 }
+
+
+// operaciones aritméticas básicas
+class OperacionesAritmeticas {
+    // Método para sumar dos números
+    public int sumar(int a, int b) {
+        return a + b;
+    }
+
+    // Método para restar dos números
+    public int restar(int a, int b) {
+        return a - b;
+    }
+
+    // Método para multiplicar dos números
+    public int multiplicar(int a, int b) {
+        return a * b;
+    }
+
+    // Método para dividir dos números
+    public double dividir(int a, int b) {
+        if (b != 0) {
+            return (double) a / b;
+        } else {
+            System.out.println("Error: División por cero");
+            return 0;
+        }
+    }
+}
