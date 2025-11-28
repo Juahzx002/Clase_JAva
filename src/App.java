926ba2ef187aa21289f0b1556576006d07f14bd6
Juan <juanurangobarrios@gmail.com>
2025-11-28 13:43:55 -0500

    feat: Operaciones de numeros enteros

--- src/App.java
+++ src/App.java
@@ -1,8 +1,17 @@
+import java.util.Scanner;
 
 // Estructura básica de un programa Java
 public class App {
     public static void main(String[] args) throws Exception {
         // Imprime el nombre
         System.out.println("Tu Nombre");
+    }
+
+}
+
+class operaciones {
+    // Método para realizar una suma
+    public int sumar(int a, int b) {
+        return a + b;
     }
 }
