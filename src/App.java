d479f2b3a8b48dff4d4f49f952287513b6bc1fcf
Juan <juanurangobarrios@gmail.com>
2025-11-28 14:57:51 -0500

    Desafio FizzBuzz

--- src/App.java
+++ src/App.java
@@ -23,13 +23,30 @@ class forLoopExample {
 }
 
 //  Explorando los Bucles While
-class whileLoopExample {
+class whileejemplo {
     public static void main(String[] args) throws Exception {
         int contador = 1;
 
         while (contador <= 5) {
             System.out.println("Contador: " + contador);
             contador++;
+        }
+    }
+}
+
+// Desafio FizzBuzz
+class FizzBuzz {
+    public static void main(String[] args) throws Exception {
+        for (int i = 1; i <= 100; i++) {
+            if (i % 3 == 0 && i % 5 == 0) {
+                System.out.println("FizzBuzz");
+            } else if (i % 3 == 0) {
+                System.out.println("Fizz");
+            } else if (i % 5 == 0) {
+                System.out.println("Buzz");
+            } else {
+                System.out.println(i);
+            }
         }
     }
 }
